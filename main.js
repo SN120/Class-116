2052a7b526459b62662b9c8260eb63553379c36e
SN120 <79451292+SN120@users.noreply.github.com>
2021-08-18 17:20:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var nose_x = "";
+var nose_y = "";
+var clown_nose = "";
+function preload(){
+    clown_nose = loadImage("https://i.postimg.cc/63V1fySS/Clown-Nose-PNG-Image.png");
+}
+function setup(){
+    canvas = createCanvas(400, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    image(video, 0, 0, 400, 300);
+    image(clown_nose, nose_x, nose_y, 40, 40);
+}
+function Snapshot(){
+    save('myFilterImage.png');
+}
+function modelLoaded(){
+    console.log("Posenet initialised");
+}
+function gotPoses(result){
+    if(result.length > 0){
+        console.log(result);
+        nose_x = result[0].pose.nose.x - 18;
+        nose_y = result[0].pose.nose.y - 18;
+        console.log("Nose x = "+ nose_x);
+        console.log("Nose y = "+ nose_y);
+    }
+}
